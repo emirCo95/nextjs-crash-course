29123cbeab8d991d6162fd0010aa54320b8c56e9
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-12-24 16:22:53 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Page = () => {
-  return <h1>Welcome to Next.js!</h1>;
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br />
+        Event You Cant&apos;t Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All in One Place
+      </p>
+    </section>
+  );
 };
 
 export default Page;
