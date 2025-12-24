a9d9ad2ae1cc797d6ee6a34ea6413c656e7d38eb
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-12-24 16:15:13 -0500

    shadcn install

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Schibsted_Grotesk, Martian_Mono } from 'next/font/google';
-import './globals.css';
+import '@/app/globals.css';
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: '--font-schibsted-grotesk',
