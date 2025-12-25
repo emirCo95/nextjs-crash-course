c81a8448347ed17c897d9fff00056fa0dbb58605
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-12-25 08:45:04 -0500

    Update EventCard.tsx

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -12,7 +12,7 @@ interface Props {
 
 const EventCard = ({ title, image, slug, location, date, time }: Props) => {
   return (
-    <Link href="/events" id="event-card">
+    <Link href={`/events/${slug}`} id="event-card">
       <Image
         src={image}
         alt={title}
@@ -20,6 +20,26 @@ const EventCard = ({ title, image, slug, location, date, time }: Props) => {
         height={300}
         className="poster"
       />
+      <div className="flex flex-row gap-2">
+        <Image src="/icons/pin.svg" alt="location" width={14} height={14} />
+        <p>{location}</p>
+
+        <div className="datetime">
+          <div>
+            <Image
+              src="/icons/calendar.svg"
+              alt="date"
+              width={14}
+              height={14}
+            />
+            <p>{date}</p>
+          </div>
+          <div>
+            <Image src="/icons/clock.svg" alt="date" width={14} height={14} />
+            <p>{time}</p>
+          </div>
+        </div>
+      </div>
       <p className="title">{title}</p>
     </Link>
   );
