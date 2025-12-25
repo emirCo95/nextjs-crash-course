5077ca88083d5105cd4fc74ac14bb0892ab4a0ca
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-12-24 19:32:19 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+import EventCard from '@/components/EventCard';
 import ExploreBtn from '@/components/ExploreBtn';
+
+const events = [
+  { image: '/images/event1.png', title: 'Event 1' },
+  { image: '/images/event2.png', title: 'Event 2' },
+];
 
 const Page = () => {
   return (
@@ -16,9 +22,11 @@ const Page = () => {
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
 
-        <ul className="events">
-          {[1, 2, 3, 4, 5].map((event) => (
-            <li key={event}>Event {event}</li>
+        <ul className="events list-none">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event} />
+            </li>
           ))}
         </ul>
       </div>
