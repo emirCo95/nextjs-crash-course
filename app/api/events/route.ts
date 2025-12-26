c9e9eacd87bca6c3c5f07033f4b6183785ece2c4
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-12-25 19:06:51 -0500

    cloudinary setup

---
+++ app/api/events/route.ts
@@ -0,0 +1,48 @@
+import Event from '@/database/event.model';
+import connectDB from '@/lib/mongodb';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const formData = await req.formData();
+
+    let event;
+
+    try {
+      event = Object.fromEntries(formData.entries());
+    } catch (e) {
+      return NextResponse.json(
+        {
+          message: 'Invalid JSON data format',
+          error: e instanceof Error ? e.message : 'Unknown',
+        },
+        { status: 400 }
+      );
+    }
+
+    const createdEvent = await Event.create(event);
+
+    return NextResponse.json(
+      {
+        message: 'Event created successfully',
+        event: createdEvent,
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json(
+      {
+        message: 'Event creation failed.',
+        error: e instanceof Error ? e.message : 'Unknown',
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
