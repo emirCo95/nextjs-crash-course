5df96d98047e5bcc0b530a0b19438b41819aac29
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-12-26 15:54:28 -0500

    Update BookEvent.tsx

--- components/BookEvent.tsx
+++ components/BookEvent.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+'use client';
+
+import { useState } from 'react';
 
 const BookEvent = () => {
-  return <div>BookEvent</div>;
-};
+  const [email, setEmail] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 1000);
+  };
+
+  return (
+    <div id="book-event">
+      {submitted ? (
+        <p className="text-sm">Thank you for booking!</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email Address</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              id="email"
+              placeholder="Enter your email"
+            />
+          </div>
+          <button type="submit" className="button-submit">
+            Submit
+          </button>
+        </form>
+      )}
+    </div>
+  );
+};
 export default BookEvent;
